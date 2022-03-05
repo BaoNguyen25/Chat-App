28a462be507b604a03b49e2b7bdc2f132d42e1e9
BaoNguyen25 <nguyenminhgiabao2501@gmail.com>
2022-03-05 13:18:21 +0700

    Working chat logs

--- website/application/static/index.js
+++ website/application/static/index.js
@@ -1,16 +1,31 @@
 $(function() {
     $('#sendBtn').bind('click', function() {
-        var text = document.getElementById("msg").value
-        console.log(text)
-        $.getJSON('/run', {txt: text},
+        var msg = document.getElementById("msg")
+        var text = msg.value
+        msg.value = ""
+
+        $.getJSON('/send_messages',
+            {txt:text},
             function(data) {
+
             });
-        return false;
     });
 });
 
-function validate(name){
-    if(name.length >= 2)
-        return true;
-    return false;
-}
+window.onload = function(){
+    var update_loop = setInterval(update,100);
+    update()
+};
+
+function update(){
+    fetch('/get_messages')
+        .then(function (response){
+            return response.json();
+        }).then(function (text){
+            var messages = "";
+            for(value of text["messages"]){
+                messages = messages + "<br > " + value;
+            }
+            document.getElementById("test").innerHTML = messages
+        });
+};
